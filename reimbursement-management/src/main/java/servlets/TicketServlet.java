58e94c9396810587677c0e4da350405d7a8e01db
Mojib Mohammad <mmohammd3@outlook.com>
2022-05-04 18:51:05 -0700

    Add ticket servlet and jsp home page

--- reimbursement-management/src/main/java/servlets/TicketServlet.java
+++ reimbursement-management/src/main/java/servlets/TicketServlet.java
@@ -1,16 +1,19 @@
 package servlets;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import daos.Dao;
 import daos.DaoFactory;
 import entities.Ticket;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name="tickets", value = "/")
 public class TicketServlet extends HttpServlet {
     private Dao<Ticket> ticketDao = DaoFactory.getTicketDao();
 
@@ -18,14 +21,13 @@ public class TicketServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
             ObjectMapper mapper = new ObjectMapper();
-            String ticket_id = req.getQueryString();
-            System.out.println(ticket_id.split("=")[1]);
-            Ticket ticket = ticketDao.get(Integer.parseInt(ticket_id.split("=")[1]));
-
-            System.out.println(ticket);
-            String jsonTicket = mapper.writeValueAsString(ticket);
-            resp.setStatus(200);
-            resp.getWriter().print(jsonTicket);
+            Ticket tickets = mapper.readValue((JsonParser) ticketDao.getAll(), Ticket.class);
+            // TODO: make this get method return a list for jsp file
+            req.setAttribute("tickets", tickets); // giving jsp file access to the tickets list
+            req.getRequestDispatcher("index.jspx").forward(req,resp);
+//            System.out.println(tickets);
+//            resp.setStatus(200);
+//            resp.getWriter().print(tickets);
         }catch (IOException ex) {
             resp.setStatus(500);
             resp.getWriter().print("Something went wrong get the resource!");
@@ -39,6 +41,8 @@ public class TicketServlet extends HttpServlet {
             ObjectMapper mapper = new ObjectMapper();
 
             Ticket jsonTicket = mapper.readValue(req.getInputStream(), Ticket.class);
+            resp.setStatus(400);
+            if(jsonTicket.getAmount() <= 0) resp.getWriter().print("amount can't be zero");
             boolean res = ticketDao.insert(jsonTicket);
             if(res) {
                 resp.setStatus(202);
@@ -53,11 +57,31 @@ public class TicketServlet extends HttpServlet {
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPut(req, resp);
+        Ticket ticket = getTicketFromReq(req, resp);
+
+        ticketDao.update(ticket);
+        resp.setStatus(200);
+        resp.getWriter().print("Successfully updated!");
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doDelete(req, resp);
+    }
+
+    private Ticket getTicketFromReq(HttpServletRequest req, HttpServletResponse res) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Ticket ticket = mapper.readValue(req.getInputStream(), Ticket.class);
+            return ticket;
+        }catch (IOException ex) {
+            try {
+                res.setStatus(500);
+                res.getWriter().print(ex.getLocalizedMessage());
+            }catch (IOException exe) {
+                System.out.println(ex.getLocalizedMessage());
+            }
+        }
+        return null;
     }
 }
--- reimbursement-management/src/main/webapp/tickets.jsp
+++ reimbursement-management/src/main/webapp/tickets.jsp
@@ -5,11 +5,9 @@
     <nav>
         <ul>
             <li><a href="/reimbursement-management/tickets.jsp">Tickets</a>
-            <li><a href="/reimbursement-management/employees.jsp">Tickets</a>
-            <li><a href="/reimbursement-management/managers.jsp">Tickets</a>
         </ul>
     </nav>
 </header>
-<h2>Welcome to Reimbursement Management System</h2>
+<h2>Welcome to Reimbursement Management System tickets page</h2>
 </body>
 </html>
